f0a5e760086d6b55de3a289f27e1e1b8382594c1
Ahmed Ikram Rao <airao@NIS-PK680s-MacBook-Pro.local>
2026-05-25 01:22:41 +0500

    CI IMPLEMENTATION

--- result/tests/tests.sh
+++ result/tests/tests.sh
@@ -7,7 +7,7 @@ done
 curl -sS -X POST --data "vote=b" http://vote > /dev/null
 
 for i in $(seq 1 30); do
-  if curl -fsS http://result | grep -q '1 vote'; then
+  if PGPASSWORD=postgres psql -h db -U postgres -d postgres -tAc "SELECT COUNT(*) FROM votes;" | grep -qx '1'; then
     echo -e "\e[42m------------"
     echo -e "\e[92mTests passed"
     echo -e "\e[42m------------"
